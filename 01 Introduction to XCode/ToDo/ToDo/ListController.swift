844a635d98606c8f31508f4e2902ae549fb9e765
Mark Tyers <marktyers@gmail.com>
2015-04-27 19:21:05 +0100

    Added automation script to ToDo app

---
+++ 01 Introduction to XCode/ToDo/ToDo/ListController.swift
@@ -0,0 +1,117 @@
+//
+//  ListController.swift
+//  ToDo
+//
+//  Created by Mark Tyers on 12/04/2015.
+//  Copyright (c) 2015 Coventry University. All rights reserved.
+//
+
+import UIKit
+
+class ListController: UITableViewController {
+    
+    var items:[String] = ["Bread", "Butter"]
+
+    @IBAction func showDialog(sender: UIBarButtonItem) {
+        println("showDialog")
+        var inputTextField: UITextField?
+        var alert:UIAlertController
+        alert = UIAlertController(title: "New Item", message: "Type item below", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+            textField.placeholder = "Item name"  // need to choose correct keyboard and capitalise first letter of each word.
+            inputTextField = textField
+        })
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            if let itemName = inputTextField?.text {
+                println(itemName)
+                self.items.append(itemName)
+                self.tableView.reloadData()
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let table:UITableView = self.tableView
+        if let cell:UITableViewCell = table.cellForRowAtIndexPath(indexPath) {
+            if cell.accessoryType == UITableViewCellAccessoryType.Checkmark {
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            } else {
+                cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            }
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ShoppingItem", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 04 More Swift/Note Well/Note Well/NSDate+Formatting.swift
@@ -0,0 +1,9 @@
+//
+//  NSDate+Formatting.swift
+//  Note Well
+//
+//  Created by Mark Tyers on 27/04/2015.
+//  Copyright (c) 2015 Coventry University. All rights reserved.
+//
+
+import Foundation
