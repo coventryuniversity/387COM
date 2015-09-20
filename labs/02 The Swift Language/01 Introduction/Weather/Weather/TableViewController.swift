5340f0e53d4f4cc47db1c3c5a81e184d44bfbdab
Mark Tyers <marktyers@gmail.com>
2015-09-20 10:44:49 +0100

    Updated Swift Lab

--- labs/02 The Swift Language/01 Introduction/Weather/Weather/TableViewController.swift
+++ labs/02 The Swift Language/01 Introduction/Weather/Weather/TableViewController.swift
@@ -12,12 +12,15 @@ struct Item {
     var date:NSDate
     var description:String
     var maxTemp:Float
+    var icon:UIImage
 }
 /* this is an enumeration that conform to the 'ErrorType' protocol. It defines different but related errors that we can throw in our code. We will spend more time with protocol in the next worksheet. */
 enum JSONError: ErrorType {
     case InvalidURL(String)
     case InvalidKey(String)
     case InvalidArray
+    case InvalidData
+    case InvalidImage
 }
 
 class TableViewController: UITableViewController {
@@ -25,72 +28,34 @@ class TableViewController: UITableViewController {
     /* Here we declare an empty array to store Item structs. */
     var days:[Item] = []
     
-    /* here we define a private function that is not visible outside the class. It takes a single String parameter and returns nothing (Void). */
-    private func useSwifty(withCity city: String) -> Void {
-        /* to make it easier to follow this code, paste the following URL into a new browser tab. The 'let' keyword creates an 'immutable' variable (sometimes called a constant). Notice that the 'city' parameter is inserted into the string.*/
-        let jsonUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=7&q=\(city)"
-        /* to find out about the NSURLSession class, click on it then open the 'Quick Help' in the right-hand panel in XCode */
-        let session = NSURLSession.sharedSession()
-        let shotsUrl = NSURL(string: jsonUrl)
-        //print(shotsUrl)
-        /* This code creates a named task. There are two parameters here: the NSURL object and an anonymous function (callback). Take a few minutes to make sense of the syntax. The second (callback) parameter is between curly braces {}. It provides three parameters (data, response and error) and returns nothing (Void). */
-        let task = session.dataTaskWithURL(shotsUrl!) { (data, response, error) -> Void in
-            //print("dataTaskWithURL")
-            do {
-                /* the callback sends the response as an NSData object. We create a new JSON object passing the NSData object. This gives us a JSON object we can parse easily. */
-                let json = JSON(data: data!)
-                let days = json["list"]
-                //print(days)
-                /* 'days' is an array of JSON objects. We need to iterate through this to extract the forecast for each day. 'index' contains the array index (stored as a String) whilst 'element' is a JSON object. */
-                for (index,element):(String, JSON) in days {
-                    /* notice how we interpolate variables within a string. */
-                    print("key: \(index)")
-                    /* There is no guarantee that the JSON data contains the required key. If this is missing a 'nul' value is returned. If this is the case we need to stop and throw an error. We need to pass a string to this particular error */
-                    guard let timestamp = element["dt"].double else {
-                        throw JSONError.InvalidKey("day[\(index)].dt.double")
-                    }
-                    /* this next line does not use a guard statement. Can you fix it? */
-                    let desc = element["weather"][0]["description"].stringValue
-                    /* we need to turn the timestamp into an NSDate. The NSDate constructor requires an NSTimeInterval object. Open the utilities (right) pane on the **Quick Help** tab then try clicking on each class (in purple) to view the documentation */
-                    let date = NSDate(timeIntervalSince1970: NSTimeInterval(timestamp))
-                    /* now we create an 'Item' struct using the data from the JSON document. Notice that the 'maxTemp' value is extracted in-line. Add code to store this value in an immutable variable, throwing an error if it can't be found. */
-                    let newDay = Item(date: date, description: desc, maxTemp: element["temp"]["max"].float!)
-                    self.days.append(newDay)
-                }
-                //print(self.days)
-                self.tableView.reloadData()
-            } catch JSONError.InvalidKey(let message) {
-                /* Here we catch a specific error. The message contains the string we passed when the error was thrown. */
-                print("A JSON Key error has ocurred: \(message)")
-            } catch is JSONError {
-                /* this block of code will catch any JSONError */
-            } catch {
-                /* this block will catch any errors not caught by the previous blocks */
-            }
-        }
-        task.resume()
-        self.tableView.reloadData()
-    }
-    
-    func useNative(withCity city: String) -> Void {
+    /* This is a function that takes a single 'String' parameter and returns nothing (Void). There are a couple of important features to note. First, all function declarations begin with the 'func' keyword. Secondly notice that Swift uses 'named parameters', in other words each parameter includes a label to describe its purpose. In our example the label is 'withCity'. Each parameter may also include a data type. here we are declaring that the parameter must be of type 'String'. The return value is indicated after the 'skinny arrow' If there is no return value the arrow and return type may be omitted. */
+    func getForecast(withCity city: String) -> Void {
+        /* when the app is retrieving data from the Internet you should display a small spinning indicator in the top status bar. */
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        /* if we are running any parts of a program that may conceivably fail these should be wrapped in a 'do' block. If an error does get triggered it will be caught in a 'catch' block that follows. */
         do {
+            /* here we generate the URL to pass to the API by inserting our 'city' parameter. Notice how we use the \() syntax to insert the variable. */
             let jsonUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=7&q=\(city)"
             print(jsonUrl)
+            /* NSURLSession provides the API for downloading content over HTTP */
             let session = NSURLSession.sharedSession()
+            /* an NSURL object contains a valid reference to an online resource. Because the string we pass it may not be valid it could fail to create the NSURL object. Because of this we need to use a 'guard' conditional. This checks if the command fails and allows us to run a block of code to handle this. In this example we will throw a custom error. These errors are defined in the 'enum' at the top of this file. The error will be handled in the second catch block below. */
             guard let shotsUrl = NSURL(string: jsonUrl) else {
                 throw JSONError.InvalidURL(jsonUrl)
             }
+            /* Here we have an example of a method that takes two parameters. The first is the NSURL object we have just created but the second parameter is a 'completion handler'. A completion handler is an anonymous function passed as a parameter. This is declared inside a pair of curly braces {}. In the example below this function has three parameters and returns 'Void'. The method 'dataTaskWithURL creates a GET request and then runs the completion handler with the results. The data parameter contains the information found at the URL stored as an NSData object. */
             let task = session.dataTaskWithURL(shotsUrl, completionHandler: {(data, response, error) -> Void in
                 do {
+                    /* This method serialises the NSData into a JSON object ready for use in the app. If this fails it automatically throws an error. By adding the 'try' keyword it sends the error to the attached 'catch' block. */
                     let json = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
                     print(json)
+                    /* we have already covered the use of the guard condition. Each time we retrieve an array or object key from the json object we can't guarantee it will be there. By extracting the data piece by piece and making use of the guard condition we can safely extract the data from the json and report any errors if things go wrong without crashing the app. The 'AnyObject' data type means days can contain any valid Swift object. */
                     guard let days:[AnyObject] = (json["list"] as! [AnyObject]) else {
                         throw JSONError.InvalidArray
                     }
-                    //print(days)
+                    /* because the previous block didn't fail we know that 'days' contains a valid array which we can now loop through using the for...in loop. */
                     for day in days {
-                        //print(day)
+                        /* We can now extract each piece of data and store it in immutable variables. */
                         guard let timestamp:Double = day["dt"] as? Double else {
                             throw JSONError.InvalidKey("dt")
                         }
@@ -99,26 +64,44 @@ class TableViewController: UITableViewController {
                         guard let weather:[AnyObject] = day["weather"] as? [AnyObject] else {
                             throw JSONError.InvalidArray
                         }
-                        //print(weather)
                         guard let desc:String = weather[0]["description"] as? String else {
                             throw JSONError.InvalidKey("description")
                         }
-                        print(desc)
+                        guard let icon:String = weather[0]["icon"] as? String else {
+                            throw JSONError.InvalidKey("icon")
+                        }
+                        guard let url = NSURL(string: "http://openweathermap.org/img/w/\(icon).png") else {
+                            throw JSONError.InvalidURL("http://openweathermap.org/img/w/\(icon).png")
+                        }
+                        guard let data = NSData(contentsOfURL: url) else {
+                            throw JSONError.InvalidData
+                        }
+                        guard let image = UIImage(data: data) else {
+                            throw JSONError.InvalidImage
+                        }
                         guard let temp:AnyObject = day["temp"] else {
                             throw JSONError.InvalidKey("temp")
                         }
-                        //print(temp)
                         guard let max:Float = temp["max"] as? Float else {
                             throw JSONError.InvalidKey("max")
                         }
-                        print(max)
-                        let newDay = Item(date: date, description: desc, maxTemp: max)
+                        /* finally we use the data we extracted from the json object and use it to create an instance of our 'Item' struct. */
+                        let newDay = Item(date: date, description: desc, maxTemp: max, icon: image)
+                        /* We can now append this to the 'days' array we declared at the top of this class. */
                         self.days.append(newDay)
                         self.tableView.reloadData()
                     }
+                /* this first catch block only catches a specific type or error. Since this error sends a String parameter we can use this to display helpful messages. */
+                } catch JSONError.InvalidKey(let message) {
+                    print("A JSON Key error has ocurred: \(message)")
+                /* This second catch block handles any JSONError errors not handled by the previous block */
+                } catch is JSONError {
+                    print("a JSON error has occurred")
+                /* This final catch handles any other errors. */
                 } catch {
                     print("Fetch failed: \((error as NSError).localizedDescription)")
                 }
+                /* Now we have finished downloading data from the Internet we can hide the 'networkActivityIndicator' */
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
             })
             task.resume()
@@ -138,8 +121,9 @@ class TableViewController: UITableViewController {
         self.tableView.delegate = self
         self.refreshControl?.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
         /* there are two popular ways to handle JSON data in Swift. There is the built-in 'native' approach but there is also a popular library called 'SwiftyJSON' which provides a number of classes to make life easier. This class contains both approaches. By commenting and uncommenting the two lines below you can work with either approach. Notice that Swift uses 'named parameters'. Compare the function definition (above) with the calling syntax. */
-        //self.useSwifty(withCity: "coventry,uk")
-        self.useNative(withCity: "coventry,uk")
+        self.getForecast(withCity: "coventry,uk")
+        /* here we can change the default row height for our UITableViewCell */
+        self.tableView.rowHeight = 66.0
         self.tableView.reloadData()
     }
 
@@ -149,26 +133,26 @@ class TableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
+    /* This is the first of the 'Data Source' methods that control what gets displayed in each of the UITableViewCell objects. This one tells the app how many cells there shound be in the UITablwView. As you can see this will match the number of indexes in the 'days' array. */
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return self.days.count
     }
 
-    
+    /* This is the second of two 'Data Source' methods. This one is where we describe what information will be displayed in a particular UITableViewCell. The app already knows how many rows to display in each section so as it needs to display a specific cell it calls this method. The second parameter 'indexPath' is an 'NSIndexPath' struct containing the 'section' and 'row' values. As you can see, this method returns a UITableViewCell containing the appropriate data. */
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        /* grabs any UITableViewCell object that is no longer visible (scrolled of screen). These are stored in a queue. If it can't find one to reuse it creates a new one. The 'cell idenfier' is a string we assigned to the UITableCell in interface builder to uniquely identify it. */
         let cell = tableView.dequeueReusableCellWithIdentifier("dailyForecast", forIndexPath: indexPath)
+        /* print out the struct for the required index */
         print(days[indexPath.row])
+        /* 'cell.textLabel' is an 'optional' value. This means it may not contain anything. If this is the case the app will crash if we try to access it (returns a 'nul' value). To prevent this we 'unwrap' it by trying to assign it to a local object, 'label'. If this succeeds we can use this local variable. If it fails then the conditional block is ignored. */
         if let label = cell.textLabel {
+            /* the date for each forecast is being stored as an NSDate object. To display this as a string in the required format we need to make use of an NSDateFormatter. We assign a code to describe how we want to display the date in its 'dateFormat' property then pass the NSDate to its 'stringFromDate()' method. This returns our formatted string. */
             let formatter = NSDateFormatter()
-            formatter.dateFormat = "EE MMM d"
+            formatter.dateFormat = "EE"
             label.text = formatter.stringFromDate(days[indexPath.row].date)
-            //label.text = days[indexPath.row].description
+        }
+        if let icon = cell.imageView {
+            icon.image = days[indexPath.row].icon
         }
         return cell
     }
--- labs/02 The Swift Language/01 Introduction/worksheet.md
+++ labs/02 The Swift Language/01 Introduction/worksheet.md
@@ -1,2 +1,72 @@
 # Introduction to Swift
-## Worksheet
+
+
+1. Parsing JSON Data with SwiftyJSON
+2. Parsing JSON Data with Native Tools
+3. Building a Custom Model Class
+4. Building a Framework
+
+
+Tasks
+
+Load the Weather Project into XCode
+
+Read through the comments and make sure you understand how the code works.
+
+- viewDidLoad (line 135)
+- useSwifty (line 29)
+- tableView cell for row at indexpath (line 164)
+
+Now run the app and make sure it works correctly (pull to refesh the tableview)
+
+## Formatting the Cell
+
+Carefully read the *Data Source* methods with the following signature:
+```
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+```
+Make sure you understand what its purpose is. In particular read through the documentation for the classes involved. You can do this by clicking on the string *NSDate* in the editor then opening the **Quick Help** inspector. There is a link at the bottom to take you to the full documentation:
+
+1. UITableView
+2. UITableViewCell
+3. NSIndexPath
+4. NSDate
+5. NSDateFormatter
+
+Currently only the date is showing in each cell. Open up the Storyboard, select the table cell and, in the attributed inspector change the style from **Basic** to **Right Detail**. Notice how in the cell-for-row-at-indexpath data source method only unwraps and sets the 
+
+Use the existing code as a guide to unwrap the **detailTextLabel**. Display the date in this and, in the main **textLabel** display the weather description and max temperature using string interpolation (there are several examples of this in the code).
+
+The date is only displayed as the day of the month. Modify the formatter's dateFormat string to display the day of the week, day of the month and month name, eg: **Mon Sept 21**. You may need to refer to the following web page:
+
+http://www.codingexplorer.com/swiftly-getting-human-readable-date-nsdateformatter/
+
+## Displaying an Image
+
+In this task you will be displaying a small image in each of the cells to summarise the weather for each day. You will be using two classes:
+
+1. UIImage
+2. UIImageView
+
+Read through the documentation for these before continuing.
+
+If you look carefully at the definition of our **Item** struct you will notice that the fourth key is called **icon** and stores a UIImage. Each cell has a built-in **UIImageView** which contains an **image** property. Based on the existing code, modify your app to display the correct weather icon in each UITableViewCell.
+
+## Working with JSON Data
+
+Most iPhone apps interact with online APIs to access useful information and to communicate with other users. In this next activity you will learn how to make GET requests over HTTP to download data in JSON format from an online API.
+
+You will be making use of a number of important classes. Take a look at their documentation before you continue.
+
+1. UIApplication
+2. NSURLSession
+3. NSURL
+4. NSJSONSerialization
+
+Locate the method `getForecast(withCity city: String) -> Void` located near the start of the `TableViewController.swift` file. Use the detailed comments to read through the code so that you understand how it works. Make sure you fully understand how the data is extracted safely from the json data.
+
+### Test Your Knowledge.
+
+
